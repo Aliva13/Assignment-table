63c947d85f94365e12048f6e97052a58aa2a9077
Aliva13 <alivamishra86@gmail.com>
2023-05-31 13:29:00 +0530

    commit

--- script.js
+++ script.js
@@ -1,30 +1,15 @@
-var viewButtons = document.getElementsByClassName('fa fa-eye');
-var userModal = document.getElementById('userModal');
-var closeButton = document.getElementById('closeButton');
 
-for (var i = 0; i < viewButtons.length; i++) {
-  viewButtons[i].addEventListener('click', function() {
-    var row = this.parentNode.parentNode;
-    var userName = row.cells[0].textContent;
-    var userType = row.cells[1].textContent;
-    var lastAccessedTime = row.cells[2].textContent;
-    var userId = '744';
-    var createdBy = 'Aliva Mishra';
-    var contactNo = '6370152655'; 
-    var emailId = 'alivamishra86@gmail.com'; 
-
-    document.getElementById('modalUserName').textContent = userName;
-    document.getElementById('modalUserType').textContent = userType;
-    document.getElementById('modalLastAccessedTime').textContent = lastAccessedTime;
-    document.getElementById('modalUserId').textContent =userId;
-    document.getElementById('modalCreatedBy').textContent = createdBy;
-    document.getElementById('modalContactNo').textContent = contactNo;
-    document.getElementById('modalEmailId').textContent = emailId;
-
-    userModal.style.display = 'block';
-  });
+function showUserDetails(userName, userId, userType, createdBy, lastAccessedTime, contactNo, emailId) {
+document.getElementById("modalUserName").innerText = userName;
+document.getElementById("modalUserId").innerText = userId;
+document.getElementById("modalUserType").innerText = userType;
+document.getElementById("modalCreatedBy").innerText = createdBy;
+document.getElementById("modalLastAccessedTime").innerText = lastAccessedTime;
+document.getElementById("modalContactNo").innerText = contactNo;
+document.getElementById("modalEmailId").innerText = emailId;
+document.getElementById("userModal").style.display = "block";
 }
 
-closeButton.addEventListener('click', function() {
-  userModal.style.display = 'none';
-});
+function closeModal() {
+document.getElementById("userModal").style.display = "none";
+}
